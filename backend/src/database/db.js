fafc16970b0f0f219b9156730b2ae99dc729ff86
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-27 20:21:39 +0530

    configured database mongoose

---
+++ backend/src/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB=async()=>{
+  try{
+    const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}`)
+
+    console.log((`mongodb connected successfully
+      :${connectionInstance.connection.host
+    }`))
+
+  }catch(error){
+    console.log("mongodb connection error",error)
+  }
+}
+export {connectDB}
