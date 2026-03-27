55f2c593c3c66edbdddc9c3407ccc7f4b8a7622e
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-27 20:22:54 +0530

    definedswrapper classes as ApiError and ApiResponse, and a wrapper handler function asyncHandler

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,18 @@
+class ApiError extends Error{
+  constructor(statusCode,message="Something went wrong",stack="",error=[]){
+    super(message)
+    this.data=null
+    this.statusCode=statusCode
+    this.message=message
+    this.errors=error
+    this.success=false
+
+ 
+    if(stack){
+      this.stack=stack
+    }else{
+      Error.captureStackTrace(this,this.constructor)
+    }
+  }
+}
+export {ApiError}
