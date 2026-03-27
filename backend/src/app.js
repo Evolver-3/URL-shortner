2738254cbd26fc4225f4908069a7b40d1b7c845c
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-27 20:29:53 +0530

    initialize express app with middleware and user routes

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
+const app=express()
+
+app.use(cors({
+  origin:process.env.CORS,
+  credentials:true
+}))
+
+app.use(express.json({
+  limit:"16kb"
+}))
+app.use(express.urlencoded({
+  extended:true,
+  limit:"16kb"
+}))
+
+app.use(cookieParser())
+
+import routes from './routes/user.routes.js'
+
+app.use("/api/v1/users",routes)
+
+
+
+export { app }
