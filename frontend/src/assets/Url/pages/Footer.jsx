8ed1a98d07449240321d65912764d1164ddea2d4
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-02 00:00:02 +0530

    made a comp for Footer with links

---
+++ frontend/src/assets/Url/pages/Footer.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from 'react'
+import Contact from './Contact'
+
+const Footer = () => {
+
+  const [openContact,setOpenContact]=useState(false)
+  return (
+    <div className='flex flex-col gap-4 items-center justify-between text-white mt-68.5 bg-gray-900 pt-4 border-t-2 border-neutral-300 pb-3 md:mt-70 '>
+
+      <div className='text-sm flex flex-col gap-2 text-neutral-400'>
+        <h2>© 2026 Shorten - A tool to shorten a long link</h2>
+      <h2 className='text-center text-md '> Made By <span className='text-red-500 text-semibold'>Ashu</span></h2>
+      </div>
+
+      <div className='flex gap-8 text-sm'>
+        <h3>Shorten</h3>
+        <h3>Terms of Service</h3>
+        <h3>Privacy</h3>
+        <h3 className='' onClick={()=>setOpenContact(!openContact)}>Contact</h3>
+      </div>
+    </div>
+
+    //  <Contact openContact={openContact}/>
+
+    
+
+   
+    
+  )
+}
+
+export default Footer
+
+
