4e7ff1b741f09889d253e0cabe377d75668f963c
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-01 23:59:00 +0530

    Add Back component for displaying URL shortening benefits

---
+++ frontend/src/assets/Url/pages/Back.jsx
@@ -0,0 +1,29 @@
+
+import Footer from './Footer'
+import Nav from './Nav'
+
+const Back = () => {
+  return (
+  
+     <div className=' text-white '>
+      
+
+      <ul className='flex flex-col gap-3 '>
+        <li  className='text-lg md:text-3xl'>Get Your Short Url today</li>
+      <div className='text-sm flex flex-col gap-1 max-w-sm px-4'>
+        <li className=''>Check how many clicks it received by users </li>
+        <li className='leading-tight'>Track statistics for your business and projects </li>
+        <li className='leading-tight'>monitoring the number of hits from your URL with our click counter </li>
+        
+      </div>
+        
+      </ul>
+     
+     </div>
+
+
+   
+  )
+}
+
+export default Back
