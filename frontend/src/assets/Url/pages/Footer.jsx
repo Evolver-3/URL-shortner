4281a948ba1239baaca252f2777d7b7f9d12dede
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-02 22:09:32 +0530

    new link on nav added for All Links

--- frontend/src/assets/Url/pages/Footer.jsx
+++ frontend/src/assets/Url/pages/Footer.jsx
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
-import Contact from './Contact'
+import {Link} from 'react-router-dom'
 
 const Footer = () => {
 
   const [openContact,setOpenContact]=useState(false)
   return (
-    <div className='flex flex-col gap-4 items-center justify-between text-white mt-68.5 bg-gray-900 pt-4 border-t-2 border-neutral-300 pb-3 md:mt-70 '>
+    <div className='flex flex-col gap-4 items-center justify-between text-white bg-gray-900 pt-4 border-t-2 border-neutral-300 pb-3 '>
 
       <div className='text-sm flex flex-col gap-2 text-neutral-400'>
         <h2>© 2026 Shorten - A tool to shorten a long link</h2>
@@ -13,10 +13,11 @@ const Footer = () => {
       </div>
 
       <div className='flex gap-8 text-sm'>
-        <h3>Shorten</h3>
+        <h3><Link to={'/'}>Shorten</Link></h3>
         <h3>Terms of Service</h3>
         <h3>Privacy</h3>
         <h3 className='' onClick={()=>setOpenContact(!openContact)}>Contact</h3>
+        <h3><Link to={"/getAll"}>Get Description</Link></h3>
       </div>
     </div>
 
