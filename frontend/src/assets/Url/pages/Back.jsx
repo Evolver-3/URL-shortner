df064c7c4a2fcbd0c52705a9d2b62b814245b170
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-02 22:10:39 +0530

    font sizes now responsive

--- frontend/src/assets/Url/pages/Back.jsx
+++ frontend/src/assets/Url/pages/Back.jsx
@@ -5,12 +5,12 @@ import Nav from './Nav'
 const Back = () => {
   return (
   
-     <div className=' text-white '>
+     <div className=' text-white mt-5'>
       
 
       <ul className='flex flex-col gap-3 '>
-        <li  className='text-lg md:text-3xl'>Get Your Short Url today</li>
-      <div className='text-sm flex flex-col gap-1 max-w-sm px-4'>
+        <li  className='text-2xl md:text-3xl lg:text-5xl px-4'>Get Your Short Url </li>
+      <div className='text-md flex flex-col gap-1 md:text-md lg:text-xl max-w-sm px-4'>
         <li className=''>Check how many clicks it received by users </li>
         <li className='leading-tight'>Track statistics for your business and projects </li>
         <li className='leading-tight'>monitoring the number of hits from your URL with our click counter </li>
