9dfa4fc91abaafa42ad84ba25fd2d7e6b4723be6
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-02 22:09:11 +0530

    new link on nav added for All Links

--- frontend/src/assets/Url/pages/Nav.jsx
+++ frontend/src/assets/Url/pages/Nav.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+
 
 const Nav = () => {
   return (
-    <div className='w-full  bg-gray-900 py-2 px-1  flex items-center justify-between'>
-      <h2 className='text-3xl md:text-4xl text-white px-10'>Shorten.</h2>
+    <div className='w-full  bg-gray-900 py-1 px-1 md:py-2 flex items-center justify-between'>
+      <h2 className='text-2xl md:text-4xl text-white px-10'>Shorten.</h2>
 
-      {/* <svg  xmlns="http://www.w3.org/2000/svg" 
-      className='w-10 h-10'
-      fill="currentColor" viewBox="0 0 24 24" >
+      <div>
+        
 
-      <path d="M3 5h18v2H3zm5 6h13v2H8zm5 6h8v2h-8z"></path>
-</svg> */}
+      </div>
+
+
     </div>
   )
 }
