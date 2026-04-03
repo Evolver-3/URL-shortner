b66d1d8eb55e224200aa84a6016a1244a5a58bac
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-03 18:48:22 +0530

    hooks created for contact page named as handleGettingMessage

--- frontend/src/assets/Url/hooks/useUrl.js
+++ frontend/src/assets/Url/hooks/useUrl.js
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { UrlContext } from "../url.context.jsx";
-import { PutUrl,getAllUrls} from "../services/url.api.js";
+import { PutUrl,getAllUrls,getMessages} from "../services/url.api.js";
 
 export const useUrl=()=>{
   
   const context=useContext(UrlContext)
 
-  const {urls,setUrls,allUrls,setAllUrls}=context
+  const {urls,setUrls,allUrls,setAllUrls,messages,setMessages}=context
 
   
 
@@ -28,20 +28,30 @@ export const useUrl=()=>{
   const handleInputAllUrl=async()=>{
     try{
       const data=await getAllUrls()
-
-  
-
        setAllUrls(data.data)
 
     }catch(error){
       console.log("Url not found", error)
       return false
        
-      
     }
   }
 
+  const handleGettingMessage=async({username,email,message})=>{
+
+    try{
+      const data=await getMessages({username,email,message})
+
+      setMessages(data.data)
+      console.log(data.data)
+      return true
+
+    }catch(error){
+      console.log("Invalid Url", error)
+      return false
+    }
+  }
   
-  return {urls,setUrls,handleInputUrl,allUrls,setAllUrls,handleInputAllUrl}
+  return {urls,setUrls,handleInputUrl,allUrls,setAllUrls,handleInputAllUrl,messages,setMessages,handleGettingMessage}
 }
 
