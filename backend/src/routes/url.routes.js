38f1c4c250d0f1f90eeae5f500416f8f9ac45323
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-03 19:14:35 +0530

    new route for contact is added and redirect route has been updated

--- backend/src/routes/url.routes.js
+++ backend/src/routes/url.routes.js
@@ -1,11 +1,21 @@
 import {Router} from 'express'
-import {updateLongUrl} from '../controllers/url.controller.js'
-import {redirectToLongUrl} from '../controllers/url.controller.js'
+import {updateLongUrl,redirectToLongUrl,getAllUrls,getContact} from '../controllers/url.controller.js'
+
 
 const urlRoute=Router()
 
 urlRoute.route("/shorten").post(updateLongUrl)
 
-urlRoute.route("/:shortId").get(redirectToLongUrl)
+urlRoute.route("/contact").post(getContact)
+
+
+urlRoute.route("/getAll").get(getAllUrls)
+
+
+urlRoute.route("/r/:shortId").get(redirectToLongUrl)
+
+
+
+
 
 export default urlRoute
