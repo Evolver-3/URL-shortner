9f539ef78ce9b85856e8c3b3fd95f7bbc5c1314c
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-03 18:46:07 +0530

    for contact new state were created for frontend

--- frontend/src/assets/Url/url.context.jsx
+++ frontend/src/assets/Url/url.context.jsx
@@ -7,8 +7,12 @@ export const UrlProvider=({children})=>{
 
   const [urls,setUrls]=useState(null)
   const [allUrls,setAllUrls]=useState(null)
+
+
+  const [messages,setMessages]=useState(null)
+
   return (
-    <UrlContext.Provider value={{urls,setUrls,allUrls,setAllUrls}}>
+    <UrlContext.Provider value={{urls,setUrls,allUrls,setAllUrls,messages,setMessages}}>
       {children}
     </UrlContext.Provider>
   )
